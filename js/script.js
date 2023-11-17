ad34219cf7fe7bba01ef695898e41e6a4a520261
SebastianBuftea <sebibuftea85@gmail.com>
2023-11-17 13:04:23 +0100

    [FINISH]

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ console.log(number);
 /* esercizio 2 */
 const PI= 3.14;
 console.log(PI); 
+
+/* esercizio 3 */
+
+/* const PI= 3.1416;
+console.log(PI);
+ */
+
+/* esercizio 4 */
+let radius =8;
+let circle= radius * 2 * PI;
+console.log(circle);
+
+/* esercizio 5 */
+let nome= `carlo`;
+nome=`Marco`;
+console.log(nome);
