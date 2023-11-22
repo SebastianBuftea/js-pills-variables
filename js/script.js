a42826346a4a4bdce7cb378fa1ffe54c9814c9b1
SebastianBuftea <sebibuftea85@gmail.com>
2023-11-22 10:35:32 +0100

    [UPDATE]

--- js/script.js
+++ js/script.js
@@ -15,25 +15,43 @@ console.log(PI);
  */
 
 /* esercizio 4 */
-let radius =8;
+/* let radius =8;
 let circle= radius * 2 * PI;
-console.log(circle);
+console.log(circle); */
 
 /* esercizio 5 */
-let nome= `carlo`;
+/* let nome= `carlo`;
 nome=`Marco`;
-console.log(nome);
+console.log(nome); */
 
 
 /* 
 esercizi del 20/11 */
-
+/* 
 let greet="Ciao, Mondo!";
-console.log(greet);
+console.log(greet); */
 
 /* let greet="Come stai, Mondo?";
 console.log(greet);
  */
-greet= greet +" "+ "Come stai";
+/* greet= greet +" "+ "Come stai";
 greet= `${greet} come stai`;
-console.log(greet);
+console.log(greet); */
+
+
+let num1=10;
+
+let comparison;
+
+if (num1 == "10"){
+  comparison= true; 
+} 
+else{
+  comparison= false;
+}
+
+ console.log(comparison);
+
+
+
+
